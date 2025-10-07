5cf7f8b674ae7683e74a6f91b895a789eb5df233
anjuhubgit <anjumhiremath196@gmail.com>
2025-10-07 20:58:31 +0530

    Add files via upload

---
+++ modules/Rds/variables.tf
@@ -0,0 +1,46 @@
+variable "engine" {
+  description = "engine"
+  type = string
+}
+
+variable "engine_version" {
+    description = "version"
+    type = string
+  
+}
+
+variable "allocated_storage" {
+    description = "storage"
+    type = string
+  
+}
+
+variable "instance_class" {
+    description = "class"
+    type = string
+  
+}
+
+variable "storage_type" {
+    description = "storage type"
+    type = string
+  
+}
+
+variable "identifier" {
+    description = "identifier"
+    type = string
+  
+}
+
+variable "username" {
+  
+  description = "usname"
+  type = string
+}
+
+variable "password" {
+    description = "password"
+    type = string
+  
+}
