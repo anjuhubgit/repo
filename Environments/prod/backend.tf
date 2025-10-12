2b8bae59191082ff6831cb5c943f3af90106f5e0
anjuhubgit <anjumhiremath196@gmail.com>
2025-10-12 23:01:45 +0530

    Update backend.tf

--- Environments/prod/backend.tf
+++ Environments/prod/backend.tf
@@ -1,11 +1,12 @@
-/*  terraform {
+  terraform {
   backend "s3" {
     bucket         = "myawsbuckethirem" # REPLACE with your S3 bucket name
-    key            = "C:/Users/a.mahesh.hiremath/Downloads/iac-cicd-main (1)/iac-cicd-main/Environments/devterraform.tfstate"
+    key            = "./Environments/dev"
     region         = "us-east-2"
     encrypt        = true
     use_lockfile = true
     #dynamodb_table = "my-terraform-state-lock" # REPLACE with your DynamoDB table name for state locking
   }
-} */
+} 
+
  
